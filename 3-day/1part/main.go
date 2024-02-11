189ce96b2a04650e3757079cd58aa4b083aa39bd
vaboliss <vabolis.ainis@gmail.com>
2024-02-11 15:08:19 +0200

    3 day solutions

--- 3-day/1part/main.go
+++ 3-day/1part/main.go
@@ -1,19 +1,54 @@
 package main
 
 import (
-	"bufio"
+	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	inFile, err := os.Open("input.txt")
+
+	bytes, err := os.ReadFile("input.txt")
+
 	if err != nil {
 		panic(err)
 	}
-	defer inFile.Close()
+	
+	input := string(bytes)
 
-	scanner := bufio.NewScanner(inFile)
-	for scanner.Scan() {
+	pathMap := make(map[string]int)
+	coordinates := []int { 0,0 }
+	startingKey := strconv.Itoa(coordinates[0]) + strconv.Itoa(coordinates[1]) 
+		
+	pathMap[startingKey] = 1
+	
+	for _, v := range input {
+		switch v {
+		case '<':
+			coordinates[1]-=1
+		case '>':
+			coordinates[1]+=1
+		case '^':
+			coordinates[0]+=1
+		case 'v':
+			coordinates[0]-=1
+		}
+
+		pathMap = safeAdd(pathMap, strconv.Itoa(coordinates[0]) + strconv.Itoa(coordinates[1]))
+
+	}
+	
+	fmt.Println(len(pathMap)) // 2081
+}
+
+func safeAdd(pathMap map[string]int, key string) map[string]int {	
+	elementAtMap, ok := pathMap[key]
+
+	if ok {
+		pathMap[key] = elementAtMap + 1
+	}else {
+		pathMap[key] = 1
 	}
 
+	return pathMap
 }
--- 3-day/2part/main.go
+++ 3-day/2part/main.go
@@ -2,18 +2,52 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
+type coord struct {
+	x, y int
+}
+
+func loadFile(input string) (*bufio.Scanner, *os.File) {
+	file, _ := os.Open(input + ".txt")
+	return bufio.NewScanner(file), file
+}
+
+func RobotAndSanta(chain string) map[coord]bool {
+	pathMap := make(map[coord]bool)
+	santa, roboSanta := coord{}, coord{}
+	pathMap[santa] = true
+
+	for i, rune := range chain {
+		c := &santa
+		if i%2 == 1 {
+			c = &roboSanta
+		}
+
+		switch rune {
+		case '^':
+			c.y++
+		case '>':
+			c.x++
+		case 'v':
+			c.y--
+		case '<':
+			c.x--
+		}
+		pathMap[*c] = true
+	}
+	return pathMap
+
+}
+
 func main() {
-	inFile, err := os.Open("input.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer inFile.Close()
 
-	scanner := bufio.NewScanner(inFile)
+	scanner, file := loadFile("input")
+	defer file.Close()
+
 	for scanner.Scan() {
+		fmt.Printf("MultiSanta %d\n", len(RobotAndSanta((scanner.Text()))))
 	}
-
 }
